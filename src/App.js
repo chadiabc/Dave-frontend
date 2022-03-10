3034f6e7e0bb5da9211a98a350e60fe6c883a558
chadiabc <chadiabc2411@hotmail.com>
2022-03-10 22:52:35 +0200

    fixed interface

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { useState, useEffect } from "react";
 import React from "react";
 import './App.css';
-import ReactDOM from 'react-dom';
 import CytoscapeComponent from 'react-cytoscapejs';
 import cytoscape from 'cytoscape';
-import coseBilkent from "cytoscape-cose-bilkent";
 import dagre from 'cytoscape-dagre';
+import Graphbox from "./graphBox";
 
-cytoscape.use( dagre );
-
+cytoscape.use(dagre);
 
 
 const cytoscapeStylesheet = [
@@ -39,7 +37,7 @@ const cytoscapeStylesheet = [
     style: {
       "curve-style": "bezier",
       "target-arrow-shape": "triangle",
-      "line-style":'straight',
+      "line-style": 'straight',
       'width': 1.5
     }
   },
@@ -64,168 +62,121 @@ const cytoscapeStylesheet = [
 ]
 
 var SERVER_URL = "http://127.0.0.1:5000"
-var graph = "null"
-var graphh = [{ data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-{ data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-{ data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-];
-
-// {"elementss":[{"data":{"id":"one","label":"Node 1"},"position":{"x":0,"y":0}},{"data":{"id":"two","label":"Node 2"},"position":{"x":100,"y":0}},{"data":{"label":"Edge from Node1 to Node2","source":"one","target":"two"}}]}
-
-
-const elementss={
-    nodes: [
-      { data: { id: 'one', label: 'Node 1' }},
-      { data: { id: 'two', label: 'Node 2' }},
-      { data: { id: 'three', label: 'Node 3' }},
-      { data: { id: 'four', label: 'Node 4' }}
-
-
-
-    ],
-    edges: [
-      {data: { source: 'one', target: 'two', label: 'Edge' }},
-      {data: { source: 'two', target: 'three', label: 'Edge' }},
-      {data: { source: 'two', target: 'four', label: 'Edge' }},
-
-
-    ]
-  }
-
-
-
-
+// var graph = "null"
 
 function App() {
   let [Notes, setNotes] = useState("");
   const [disableVisNow, setDisable] = React.useState(false);
   const [show, setShow] = useState(false);
+  const [graph,setGraph] = useState("null");
 
-  function getNote() {
-    fetch(`${SERVER_URL}/getnote`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)                     
-    });
-   }
-   function getgraph(){
+  function getgraph() {
     fetch(`${SERVER_URL}/getgraph`)
-    .then(response => {
-    
-    response.json().then(data => {
-      graph=data.elementss;
-      console.log(graph)
-      
-  
-  })
-  
-    
+      .then(response => {
+        response.json().then(data => {
+          setGraph(data.elementss);
+          // graph = data.elementss;
+          console.log(graph)
+        })
+      });
+  }
 
-  });
-}
+  // console.log(graph)
 
-console.log(graph)
-  //  getNote();
-  getgraph();
-  useEffect(getgraph,[])
-  
 
-   function checkKeyChanged(e){
-     if(!disableVisNow)
-     if(e.code === 'Space' || e.code === 'Enter') {
-       console.log(Notes);
-    postData(`${SERVER_URL}/Addnote`, {text: Notes});
-     }
-
-   }
-   function visualizeNow() {
+  function checkKeyChanged(e) {
+    if (!disableVisNow)
+      if (e.code === 'Space' || e.code === 'Enter') {
+        console.log(Notes);
+        postData(`${SERVER_URL}/Addnote`, { text: Notes });
+        getgraph();
+      }
+  }
+  function visualizeNow() {
     console.log(Notes);
-    postData(`${SERVER_URL}/Addnote`, {text: Notes});
-   }
-   
-
+    postData(`${SERVER_URL}/Addnote`, { text: Notes });
+  }
   
 
 
-   function visualizeNowModefunction() {
+
+
+  function visualizeNowModefunction() {
     console.log("at the end");
-    setShow(prev => !prev)
+    // setShow(prev => !prev);
     setDisable(true)
     // setVisualizeNowMode(true);
 
   }
   function FromEPIC() {
     fetch(`${SERVER_URL}/getnoteepic`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data) 
-    
-   });
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+
+      });
   }
-   function visualizeContinouslyModefunction() {
+  function visualizeContinouslyModefunction() {
     console.log("dynamic");
     setDisable(false)
-   }
+  }
 
 
-async function postData(url='',data={}){
-  const response = await fetch(url, {
-   
-method: 'POST',
-headers: {
-  'Content-Type': 'application/json'
-},
-body: JSON.stringify(data)
-});
-return response.json();
-}
+  async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
 
   return (
     <div className="App">
-        <div className="header">
-            <h1> DAVE</h1>
-        </div>
-      
-        <div className="split right">
-          <div className="centered">
-            {graph != "null" &&
-        
-        
-        <CytoscapeComponent cy={(cy) => {
-        cy.on("select", (_x) => {
-          console.log("something was selected here");
-        });
-      }} elements={CytoscapeComponent.normalizeElements(graph)} layout={{
-     name: "dagre",
-     // other options
-     padding: 10,
-     idealEdgeLength: 10,
-     edgeElasticity: 0.1
-
-   }} 
-   style={ {  left:100, top: 0, bottom: 0, position: "absolute", width: "100%" } } stylesheet={cytoscapeStylesheet}/>
-  }
-  </div>
-</div>
-  
-        <div className="wrapper">
-        <div className="split left">
-        <button id="now-mode-button" disabled={disableVisNow} className="button" type="button" onClick={visualizeNowModefunction}>Visualize at the end</button> |
-        <button id="continous-mode-button" disabled={!disableVisNow} className="button" type="button" onClick={visualizeContinouslyModefunction}>Visualize Continously</button>
-        <hr />
+      <div className="header">
+        <h1> DAVE</h1>
+      </div>
+      <div className="graphBox">
+        <div className="graphBoxLeft">
+          <div className="wrapper">
+            <button id="now-mode-button" disabled={disableVisNow} className="button" type="button" onClick={visualizeNowModefunction}>Visualize at the end</button> |
+            <button id="continous-mode-button" disabled={!disableVisNow} className="button" type="button" onClick={visualizeContinouslyModefunction}>Visualize Continously</button>
+            <hr />
             <div>
-                <label htmlFor="clincalNotesTextField">Write your notes here</label>
-                <textarea id="clincalNotesTextField" name="clincalNotesTextField" rows="15" cols="100" 
-                value={Notes} onChange={e =>setNotes(e.target.value)} onKeyPress={(e) => checkKeyChanged(e)}>
-  </textarea>
-  <button id="visualize-button" disabled={!disableVisNow} className="button" type="button" onClick={visualizeNow}>Visualize Now</button>
-  <button id="visualize-button" className="button" type="button" onClick={FromEPIC}>From EPIC</button>
-</div>
-               </div>
+              <label htmlFor="clincalNotesTextField">Write your notes here</label>
+              <textarea id="clincalNotesTextField" name="clincalNotesTextField" rows="15" cols="100"
+                value={Notes} onChange={e => setNotes(e.target.value)} onKeyPress={(e) => checkKeyChanged(e)}>
+              </textarea>
+              <button id="visualize-button" disabled={!disableVisNow} className="button" type="button" onClick={visualizeNow}>Visualize Now</button>
+              <button id="visualize-button" className="button epic" type="button" onClick={FromEPIC}>From EPIC</button>
+            </div>
+          </div>
         </div>
+
+        <div className="graphBoxRight">
+          {graph != "null" &&
+            <CytoscapeComponent className="cyto" cy={(cy) => {
+              cy.on("select", (_x) => {
+                console.log("something was selected here");
+              });
+            }} elements={CytoscapeComponent.normalizeElements(graph)} layout={{
+              name: "dagre",
+              // other options
+              padding: 10,
+              idealEdgeLength: 10,
+              edgeElasticity: 0.1
+
+            }}
+            stylesheet={cytoscapeStylesheet} />
+          }
         </div>
-   
-  
+      </div>
+    </div>
+
+
   );
 
 
