bbaafd0b6fe0b9a05469965aae6980a2779ce42f
Hadi Hamoud <hadihamoud@Hadis-MacBook-Pro.local>
2022-04-11 11:57:23 +0300

    added highlighting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,31 @@ import dagre from 'cytoscape-dagre';
 import Graphbox from "./graphBox";
 import { useRef } from "react";
 
+
+//var expandCollapse = require("cytoscape-expand-collapse");
 cytoscape.use(dagre);
+
+var options = {
+  layoutBy: null, // to rearrange after expand/collapse. It's just layout options or whole layout function. Choose your side!
+  // recommended usage: use cose-bilkent layout with randomize: false to preserve mental map upon expand/collapse
+  fisheye: true, // whether to perform fisheye view after expand/collapse you can specify a function too
+  animate: true, // whether to animate on drawing changes you can specify a function too
+  animationDuration: 1000, // when animate is true, the duration in milliseconds of the animation
+  ready: function () { }, // callback when expand/collapse initialized
+  undoable: true, // and if undoRedoExtension exists,
+
+  cueEnabled: true, // Whether cues are enabled
+  expandCollapseCuePosition: 'top-left', // default cue position is top left you can specify a function per node too
+  expandCollapseCueSize: 12, // size of expand-collapse cue
+  expandCollapseCueLineSize: 8, // size of lines used for drawing plus-minus icons
+  expandCueImage: undefined, // image of expand icon if undefined draw regular expand cue
+  collapseCueImage: undefined, // image of collapse icon if undefined draw regular collapse cue
+  expandCollapseCueSensitivity: 1, // sensitivity of expand-collapse cues
+  edgeTypeInfo: "edgeType", // the name of the field that has the edge type, retrieved from edge.data(), can be a function, if reading the field returns undefined the collapsed edge type will be "unknown"
+  groupEdgesOfSameTypeOnCollapse : false, // if true, the edges to be collapsed will be grouped according to their types, and the created collapsed edges will have same type as their group. if false the collapased edge will have "unknown" type.
+  allowNestedEdgeCollapse: true, // when you want to collapse a compound edge (edge which contains other edges) and normal edge, should it collapse without expanding the compound first
+  zIndex: 999 // z-index value of the canvas in which cue ımages are drawn
+};
 
 
 const cytoscapeStylesheet = [
@@ -59,7 +83,8 @@ const cytoscapeStylesheet = [
 
       // "text-rotation": "autorotate"
     }
-  }
+  },
+  
 ]
 
 var SERVER_URL = "http://127.0.0.1:5000"
@@ -101,7 +126,7 @@ function App() {
   }
   function visualizeNow() {
     console.log(Notes);
-    postData(`${SERVER_URL}/Addnote`, { text: Notes });
+    postData(`${SERVER_URL}/AddnoteNow`, { text: Notes });
   }
   
 
@@ -157,8 +182,14 @@ function App() {
       },
       body: JSON.stringify(data)
     })
+    setGraph("null");
     const datar = await response.json();
     setGraph(datar.elementss);
+    cytoRef.current.nodes(datar.topNode).style('background-color', '#00ffff');
+    // cytoRef.current.expandCollapse(options);
+    // var api = cytoRef.current.expandCollapse('get');
+    // api.collapseAll(options);
+    
   }
 
   return (
